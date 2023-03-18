834bec4467cfb3172a000049432d527ad3114afa
Polly Zhou <polly@Pollys-MacBook-Pro.local>
2023-03-18 10:57:51 +0800

    finish

--- exp1/hilbert.m
+++ exp1/hilbert.m
@@ -38,35 +38,35 @@ for n=10:15
     
     % Solve in gauss elimination solution
     [Y1]=gauss(gaussH,gauss_bn,n);
-    res1=norm(Y1-X);
+    res1=mse(Y1-X);
     fprintf("Y1=\n");
     disp(Y1);
     fprintf("Residual 1 = %d\n", res1);
 
     % Solve in Cholesky Factorization
     Y2=cholesky(choleH,chole_bn,n);
-    res2=norm(Y2-X);
+    res2=mse(Y2-X);
     fprintf("Y2=\n");
     disp(Y2);
     fprintf("Residual 2 = %d\n", res2);
 
     % Solve in Tikhonov Regularization
     Y3=tikhonov(tikhoH,tikho_bn,n);
-    res3=norm(Y3-X);
+    res3=mse(Y3-X);
     fprintf("Y3=\n");
     disp(Y3);
     fprintf("Residual 3 = %d\n", res3);
 
     % Solve in Conjugate Gradient Methods
     Y4=cgs(conjgradH,conjgrad_bn,9.3e-10);
-    res4=norm(Y4-X);
+    res4=mse(Y4-X);
     fprintf("Y4=\n");
     disp(Y4);
     fprintf("Residual 4 = %d\n", res4);
 
     % Solve in GMRES Methods
     Y5=gmres(gmresH,gmres_bn);
-    res5=norm(Y5-X);
+    res5=mse(Y5-X);
     fprintf("Y5=\n");
     disp(Y5);
     fprintf("Residual 5 = %d\n", res5);
@@ -81,18 +81,85 @@ for n=10:15
 end
 
 % Plot
+subplot(2,3,1);
 plot(n_arr,res_arr1,":",'LineWidth',2);hold on
 plot(n_arr,res_arr2,"-o",'LineWidth',2);hold on
 plot(n_arr,res_arr3,"-.",'LineWidth',2);hold on
 plot(n_arr,res_arr4,"-*",'LineWidth',2);hold on
 plot(n_arr,res_arr5,"-v",'LineWidth',2);legend('Gauss消去法','Cholesky分解法','Tikhonov正则化法','共轭梯度法','GMRES法');
 xlim([10,15]);
-ylim([0,0.6]);
+ylim([0,0.001]);
 set(gca,'XTick',10:1:15);
 xlabel('n');
 ylabel('Res');
-title('各种求解方法的误差随n的变化关系图');
+title('各种求解方法的误差随n的变化关系图(1E-03量级)');
+
+subplot(2,3,2);
+plot(n_arr,res_arr1,":",'LineWidth',2);hold on
+plot(n_arr,res_arr2,"-o",'LineWidth',2);hold on
+plot(n_arr,res_arr3,"-.",'LineWidth',2);hold on
+plot(n_arr,res_arr4,"-*",'LineWidth',2);hold on
+plot(n_arr,res_arr5,"-v",'LineWidth',2);legend('Gauss消去法','Cholesky分解法','Tikhonov正则化法','共轭梯度法','GMRES法');
+xlim([10,15]);
+ylim([0,0.01]);
+set(gca,'XTick',10:1:15);
+xlabel('n');
+ylabel('Res');
+title('各种求解方法的误差随n的变化关系图(1E-02量级)');
+
+subplot(2,3,3);
+plot(n_arr,res_arr1,":",'LineWidth',2);hold on
+plot(n_arr,res_arr2,"-o",'LineWidth',2);hold on
+plot(n_arr,res_arr3,"-.",'LineWidth',2);hold on
+plot(n_arr,res_arr4,"-*",'LineWidth',2);hold on
+plot(n_arr,res_arr5,"-v",'LineWidth',2);legend('Gauss消去法','Cholesky分解法','Tikhonov正则化法','共轭梯度法','GMRES法');
+xlim([10,15]);
+ylim([0,0.1]);
+set(gca,'XTick',10:1:15);
+xlabel('n');
+ylabel('Res');
+title('各种求解方法的误差随n的变化关系图(1E-01量级)');
+
+subplot(2,3,4);
+plot(n_arr,res_arr1,":",'LineWidth',2);hold on
+plot(n_arr,res_arr2,"-o",'LineWidth',2);hold on
+plot(n_arr,res_arr3,"-.",'LineWidth',2);hold on
+plot(n_arr,res_arr4,"-*",'LineWidth',2);hold on
+plot(n_arr,res_arr5,"-v",'LineWidth',2);legend('Gauss消去法','Cholesky分解法','Tikhonov正则化法','共轭梯度法','GMRES法');
+xlim([10,15]);
+ylim([0,1]);
+set(gca,'XTick',10:1:15);
+xlabel('n');
+ylabel('Error');
+title('各种求解方法的误差随n的变化关系图(1E+00量级)');
+
+subplot(2,3,5);
+plot(n_arr,res_arr1,":",'LineWidth',2);hold on
+plot(n_arr,res_arr2,"-o",'LineWidth',2);hold on
+plot(n_arr,res_arr3,"-.",'LineWidth',2);hold on
+plot(n_arr,res_arr4,"-*",'LineWidth',2);hold on
+plot(n_arr,res_arr5,"-v",'LineWidth',2);legend('Gauss消去法','Cholesky分解法','Tikhonov正则化法','共轭梯度法','GMRES法');
+xlim([10,15]);
+ylim([0,10]);
+set(gca,'XTick',10:1:15);
+xlabel('n');
+ylabel('Error');
+title('各种求解方法的误差随n的变化关系图(1E+01量级)');
+
+subplot(2,3,6);
+plot(n_arr,res_arr1,":",'LineWidth',2);hold on
+plot(n_arr,res_arr2,"-o",'LineWidth',2);hold on
+plot(n_arr,res_arr3,"-.",'LineWidth',2);hold on
+plot(n_arr,res_arr4,"-*",'LineWidth',2);hold on
+plot(n_arr,res_arr5,"-v",'LineWidth',2);legend('Gauss消去法','Cholesky分解法','Tikhonov正则化法','共轭梯度法','GMRES法');
+xlim([10,15]);
+ylim([0,10000000]);
+set(gca,'XTick',10:1:15);
+xlabel('n');
+ylabel('Error');
+title('各种求解方法的误差随n的变化关系图(1E+07量级)');
 
 
+sgtitle('不同量级下各种求解方法的误差随矩阵阶数的变化关系图');
 
 
--- exp1/tikhonov.m
+++ exp1/tikhonov.m
@@ -2,7 +2,7 @@
 % Tikhonov regularization
 %%%%%%%%%%%%%%%%%%%%%%%%%
 function [x]=tikhonov(A,b,n)
-    alpha=0.000001;
+    alpha=0.00000001;
     regular_A=alpha*eye(n)+A'*A;
     regular_b=A'*b;
     x=regular_A\regular_b;
